096080cde3d15baf179168e9487ab6a791298b73
tschaffter <thomas.schaffter@gmail.com>
2021-07-07 08:16:19 -0700

    Add component challenge-list-item

--- src/app/pages/challenges/challenges.module.ts
+++ src/app/pages/challenges/challenges.module.ts
@@ -4,6 +4,7 @@ import { FooterModule } from '@sage-bionetworks/sage-angular';
 import { MaterialModule } from '../../components/material/material.module';
 import { ChallengesComponent } from './challenges.component';
 import { ChallengeListComponent } from './challenge-list/challenge-list.component';
+import { ChallengeListItemComponent } from './challenge-list-item/challenge-list-item.component';
 import { ChallengesRoutingModule } from './challenges-routing.module';
 import { ChallengeDataService } from './challenge-data.service';
 import { ChallengeNewComponent } from './challenge-new/challenge-new.component';
@@ -18,6 +19,7 @@ import { ChallengeNewComponent } from './challenge-new/challenge-new.component';
   declarations: [
     ChallengesComponent,
     ChallengeListComponent,
+    ChallengeListItemComponent,
     ChallengeNewComponent
   ],
   providers: [
