a203f1ae336bbb1ef4694f4e861f0bfc5ed028b6
Rongrong Chai <rchai@sagebase.org>
2021-07-19 13:18:54 -0400

    save app version to env

--- src/environments/setenv.ts
+++ src/environments/setenv.ts
@@ -9,6 +9,7 @@ const SERVER_PROTOCOL = process.env.SERVER_PROTOCOL || 'http://';
 const SERVER_DOMAIN = process.env.SERVER_DOMAIN || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || '4200';
 const SERVER_BASE_PATH = process.env.SERVER_BASE_PATH || '/api';
+const APP_VERSION = process.env.APP_VERSION || '';
 
 // check whether app is running for prod; check package.json
 const targetPath = isProduction
@@ -20,7 +21,7 @@ const envConfigFile = `
   export const environment = {
       production: ${isProduction},
       apiBasePath: '${SERVER_PROTOCOL}${SERVER_DOMAIN}:${SERVER_PORT}${SERVER_BASE_PATH}',
-      appVersion: '0.1.0',
+      appVersion: '${APP_VERSION}',
       seedDatabase: !${isProduction}
   };
 `;
