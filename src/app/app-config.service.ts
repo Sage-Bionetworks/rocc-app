12a699dbc8eecee589fd6e72ead68214fa5f38c3
tschaffter <thomas.schaffter@gmail.com>
2021-07-27 16:08:16 -0700

    Cleanup AppConfigService

--- src/app/app-config.service.ts
+++ src/app/app-config.service.ts
@@ -1,41 +1,16 @@
-// import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { AppConfig, APP_CONFIG } from './app.config';
-// import { Observable } from 'rxjs';
-// import { shareReplay, tap } from 'rxjs/operators';
-
-// export enum Environment {
-//   Production = 'prod',
-//   Staging = 'staging',
-//   Test = 'test',
-//   Development = 'dev',
-//   Local = 'local',
-// }
-
-// export interface Configuration {
-//   type: Environment;
-//   apiUrl: string;
-// }
 
 @Injectable({ providedIn: 'root' })
 export class AppConfigService {
-  // private readonly configUrl = 'config/config.json';
-  // private configuration$!: Observable<Configuration>;
+  private readonly appConfig!: AppConfig;
 
-  constructor(@Inject(APP_CONFIG) private config: AppConfig) {}
-
-  getAppConfig(): Observable<AppConfig> {
-    return of(this.config);
+  constructor(@Inject(APP_CONFIG) private config: AppConfig) {
+    this.appConfig = config;
   }
 
-  // public loadAppConfig(): Observable<Configuration> {
-  //   if (!this.configuration$) {
-  //     this.configuration$ = this.http.get<Configuration>(this.configUrl).pipe(
-  //       tap(res => console.log('Reading configuration', res)),
-  //       shareReplay(1)
-  //     );
-  //   }
-  //   return this.configuration$;
-  // }
+  getAppConfig(): Observable<AppConfig> {
+    return of(this.appConfig);
+  }
 }
