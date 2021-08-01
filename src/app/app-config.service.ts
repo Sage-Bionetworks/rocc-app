afe40a404efb7ac0df9d3dcf597c25ee3d4be672
Thomas Schaffter <thomas.schaffter@gmail.com>
2021-08-01 10:13:27 -0700

    Enable to specify environment variables after building the app (#144)
    * Read environment from asset file
    
    * Add new config file system
    
    * Cleanup AppConfigService
    
    * Add docker-entrypoint.sh
    
    * Commit changes
    
    * Can now configure the app config using env vars
    
    * Stack works but can be optimized
    
    * Refactor .env
    
    * Remove dedicated nginx reverse proxy
    
    * Cleanup

---
+++ src/app/app-config.service.ts
@@ -0,0 +1,16 @@
+import { Inject, Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { AppConfig, APP_CONFIG } from './app.config';
+
+@Injectable({ providedIn: 'root' })
+export class AppConfigService {
+  private readonly appConfig!: AppConfig;
+
+  constructor(@Inject(APP_CONFIG) private config: AppConfig) {
+    this.appConfig = this.config;
+  }
+
+  getAppConfig(): Observable<AppConfig> {
+    return of(this.appConfig);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NavbarModule, FooterModule } from '@sage-bionetworks/sage-angular';
-import { ApiModule, Configuration, ConfigurationParameters } from '@sage-bionetworks/rocc-client-angular';
+import {
+  ApiModule,
+  Configuration,
+  ConfigurationParameters,
+} from '@sage-bionetworks/rocc-client-angular';
 import { BASE_PATH } from '@sage-bionetworks/rocc-client-angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { environment } from '../environments/environment';
 import { DatabaseSeedModule } from './components/database-seed/database-seed.module';
 import { FiltersModule } from './components/filters/filters.module';
+import { AppConfigService } from './app-config.service';
+import { AppConfig, APP_CONFIG } from './app.config';
 
 export function apiConfigFactory(): Configuration {
   const params: ConfigurationParameters = {
@@ -30,14 +35,23 @@ export function apiConfigFactory(): Configuration {
     FooterModule,
     ApiModule.forRoot(apiConfigFactory),
     DatabaseSeedModule,
-    FiltersModule
+    FiltersModule,
   ],
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   providers: [
-    { provide: BASE_PATH, useValue: environment.apiBasePath }
+    {
+      provide: BASE_PATH,
+      useFactory: (config: AppConfig) => config.apiUrl,
+      deps: [APP_CONFIG],
+    },
+    // {
+    //   provide: APP_INITIALIZER,
+    //   useFactory: (appConfigService: AppConfigService) => () =>
+    //     appConfigService.loadAppConfig().toPromise(),
+    //   deps: [AppConfigService, APP_CONFIG],
+    //   multi: true,
+    // },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
