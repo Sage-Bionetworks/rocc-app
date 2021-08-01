3b4213bc99bcc093bdb3dc62fd2d610b305fa08b
tschaffter <thomas.schaffter@gmail.com>
2021-07-31 19:59:59 -0700

    Stack works but can be optimized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,28 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 // TODO: fix import from @sage-bionetworks/sage-angular
 import { Section } from '@sage-bionetworks/sage-angular/src/lib/navbar/section';
+import { AppConfigService } from './app-config.service';
 import { SECTIONS } from './app-sections';
 // import { environment } from '../environments/environment';
 
 @Component({
-    selector: 'rocc-app',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    encapsulation: ViewEncapsulation.None
+  selector: 'rocc-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent implements OnInit {
   title = 'ROCC';
-  version = 'x.y.z';  // environment.appVersion;
   sections: { [key: string]: Section } = SECTIONS;
-  seedDatabase = false;  // environment.seedDatabase;
+  appVersion = 'x.y.z';
+  seedDatabase = false;
 
-  constructor() {}
+  constructor(private appConfigService: AppConfigService) {
+    this.appConfigService.getAppConfig().subscribe((config) => {
+      this.appVersion = config.appVersion;
+      this.seedDatabase = config.seedDatabase;
+    });
+  }
 
   ngOnInit(): void {}
 }
