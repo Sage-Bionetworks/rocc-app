bd18de96a5a5542e48e63e84eddc2bdff2794646
tschaffter <thomas.schaffter@gmail.com>
2021-08-31 11:10:18 -0700

    Add email and username fields to signup form

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,12 +1,78 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { PageTitleService } from '@sage-bionetworks/sage-angular';
 
 @Component({
   selector: 'rocc-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
   @HostBinding('class.main-content') readonly mainContentClass = true;
 
-  constructor() {}
+  signupForm!: FormGroup;
+  errors = {
+    email: undefined,
+    other: undefined,
+  };
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private pageTitleService: PageTitleService
+  ) {}
+
+  ngOnInit(): void {
+    this.pageTitleService.setTitle('Join ROCC • ROCC');
+
+    this.signupForm = this.formBuilder.group({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(25),
+        // forbiddenNameValidator(/bob/i) // <-- Here's how you pass in the custom
+      ]),
+
+      email: ['', [Validators.required, Validators.email]],
+      // passwordGroup: formBuilder.group({
+      //   password: ['', [Validators.required]],
+      //   confirmPassword: ['', [Validators.required]],
+      // }), // , { validator: PasswordValidation.matchPassword }
+    });
+  }
+
+  get username() {
+    return this.signupForm.get('username');
+  }
+
+  get email() {
+    return this.signupForm.get('email');
+  }
+
+  getUsernameErrorMessage(): string {
+    if (
+      this.username?.hasError('required') ||
+      this.username?.hasError('minlength') ||
+      this.username?.hasError('maxlength')
+    ) {
+      return 'A username between 3 and 64 characters is required.';
+    }
+    return '';
+  }
+
+  getEmailErrorMessage(): string {
+    if (this.email?.hasError('required') || this.email?.hasError('email')) {
+      return 'You must enter a valid email';
+    }
+    return '';
+  }
+
+  createUserAccount(): void {}
 }
--- src/app/pages/signup/signup.module.ts
+++ src/app/pages/signup/signup.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FooterModule } from '@sage-bionetworks/sage-angular';
+import { MaterialModule } from '@shared/material/material.module';
 import { SignupComponent } from './signup.component';
 
 const routes: Routes = [
@@ -9,12 +11,15 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [SignupComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes),
-    FooterModule
+    FooterModule,
+    MaterialModule
   ],
+  declarations: [SignupComponent],
   exports: [SignupComponent]
 })
 export class SignupModule {}
