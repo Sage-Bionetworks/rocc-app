f4578bf12ef3389cd8392b3c7e0b33db56144e08
Rongrong Chai <rchai@sagebase.org>
2021-09-01 10:01:13 -0400

    prettier-formatter files

--- src/app/pages/challenges/challenge-view/challenge-view.component.ts
+++ src/app/pages/challenges/challenge-view/challenge-view.component.ts
@@ -3,12 +3,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 import {
   ChallengeService,
   OrganizationService,
-  PersonService
+  PersonService,
 } from '@sage-bionetworks/rocc-client-angular';
 import {
   Challenge,
   Organization,
-  Person
+  Person,
 } from '@sage-bionetworks/rocc-client-angular';
 import { forkJoin } from 'rxjs';
 import { switchMap, tap } from 'rxjs/operators';
@@ -20,7 +20,6 @@ import { PageTitleService } from '@shared/page-title/page-title.service';
   styleUrls: ['./challenge-view.component.scss'],
 })
 export class ChallengeViewComponent implements OnInit {
-
   @Input() selected!: boolean;
   @Output() selectedChange = new EventEmitter<boolean>();
 
@@ -39,7 +38,6 @@ export class ChallengeViewComponent implements OnInit {
     private pageTitleService: PageTitleService
   ) {}
 
-
   ngOnInit(): void {
     // this.challenge$ = this.route.params.pipe(
     //   switchMap((params) => this.challengeService.getChallenge(params.id))
@@ -47,46 +45,53 @@ export class ChallengeViewComponent implements OnInit {
     // this.challenge$.subscribe(challenge => {
     //   this.pageTitleService.setTitle(challenge.name);
     // });
-    this.route.params.pipe(
-      switchMap((params) => this.challengeService.getChallenge(params.id)),
-      tap(challengeRes => {
-        this.challenge = challengeRes;
-        this.pageTitleService.setTitle(challengeRes.name);
-      }),
-      switchMap(challenge => 
-        forkJoin(
-          challenge.organizerIds
-            .map(organizerId => this.personService.getPerson(organizerId))
-        )
-      ),
-      tap((persons)=> this.personList = persons),
-      switchMap((persons) => {
-        const orgIds = [...new Set(persons.flatMap(person => person.organizationIds))]
-        return  forkJoin(
-          orgIds
-            .map((orgId) => this.organizationService.getOrganization(orgId))
-        )
-      }),
-      tap(org => this.orgList = org)
-    ).subscribe(
-      () => console.log("done")
-    );
+    this.route.params
+      .pipe(
+        switchMap((params) => this.challengeService.getChallenge(params.id)),
+        tap((challengeRes) => {
+          this.challenge = challengeRes;
+          this.pageTitleService.setTitle(challengeRes.name);
+        }),
+        switchMap((challenge) =>
+          forkJoin(
+            challenge.organizerIds.map((organizerId) =>
+              this.personService.getPerson(organizerId)
+            )
+          )
+        ),
+        tap((persons) => (this.personList = persons)),
+        switchMap((persons) => {
+          const orgIds = [
+            ...new Set(persons.flatMap((person) => person.organizationIds)),
+          ];
+          return forkJoin(
+            orgIds.map((orgId) =>
+              this.organizationService.getOrganization(orgId)
+            )
+          );
+        }),
+        tap((org) => (this.orgList = org))
+      )
+      .subscribe(() => console.log('done'));
   }
 
   getProgress(challenge: Challenge): number {
-
-    if (challenge.status == "active") {
+    if (challenge.status == 'active') {
       // if either of start/endDates not provided, still show progress bar, but return zero
-      if ((typeof challenge.startDate !== "undefined") && (typeof challenge.endDate !== "undefined")) {
-        const totalTime = +new Date(challenge.endDate) - +new Date(challenge.startDate)
-        const runTime = +new Date - +new Date(challenge.startDate)
-        this.progressValue = runTime/totalTime * 100
+      if (
+        typeof challenge.startDate !== 'undefined' &&
+        typeof challenge.endDate !== 'undefined'
+      ) {
+        const totalTime =
+          +new Date(challenge.endDate) - +new Date(challenge.startDate);
+        const runTime = +new Date() - +new Date(challenge.startDate);
+        this.progressValue = (runTime / totalTime) * 100;
       }
     } else {
-      this.progressValue = challenge.status == 'completed' ? 100 : 0
+      this.progressValue = challenge.status == 'completed' ? 100 : 0;
     }
 
-    return this.progressValue
+    return this.progressValue;
   }
 
   public toggleSelected() {
