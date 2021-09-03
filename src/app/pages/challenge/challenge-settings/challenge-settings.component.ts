6c3146823963b866195bc405ca705ae726bb6090
tschaffter <thomas.schaffter@gmail.com>
2021-09-02 19:45:29 -0700

    Bring challenge data to challenge settings

--- src/app/pages/challenge/challenge-settings/challenge-settings.component.ts
+++ src/app/pages/challenge/challenge-settings/challenge-settings.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Challenge } from '@sage-bionetworks/rocc-client-angular';
+import { ChallengeDataService } from '../challenge-data.service';
 
 @Component({
   selector: 'rocc-challenge-settings',
   templateUrl: './challenge-settings.component.html',
-  styleUrls: ['./challenge-settings.component.scss']
+  styleUrls: ['./challenge-settings.component.scss'],
 })
 export class ChallengeSettingsComponent implements OnInit {
+  challenge!: Challenge | undefined;
 
-  constructor() { }
+  constructor(private challengeDataService: ChallengeDataService) {}
 
   ngOnInit(): void {
+    this.challengeDataService.getChallenge().subscribe((challenge) => {
+      this.challenge = challenge;
+    });
   }
-
 }
