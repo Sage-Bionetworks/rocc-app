59ab75fab5e4305de8c05a48eec5ae10c05b9961
tschaffter <thomas.schaffter@gmail.com>
2021-09-03 17:08:58 -0700

    Fix linter issues

--- src/app/pages/account/user-account/user-profile-challenges/user-profile-challenges.component.ts
+++ src/app/pages/account/user-account/user-profile-challenges/user-profile-challenges.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'rocc-user-profile-challenges',
   templateUrl: './user-profile-challenges.component.html',
-  styleUrls: ['./user-profile-challenges.component.scss']
+  styleUrls: ['./user-profile-challenges.component.scss'],
 })
-export class UserProfileChallengesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class UserProfileChallengesComponent {
+  constructor() {}
 }
