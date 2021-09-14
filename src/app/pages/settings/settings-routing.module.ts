b54e0e549835dccc90a2279daf549882d72b930f
tschaffter <thomas.schaffter@gmail.com>
2021-09-14 16:34:15 -0700

    Add auth-guard

--- src/app/pages/settings/settings-routing.module.ts
+++ src/app/pages/settings/settings-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '@shared/auth/auth-guard.service';
 import { SettingsComponent } from './settings.component';
 
 const routes: Routes = [
-  { path: '', component: SettingsComponent },
+  { path: '', component: SettingsComponent, canActivate: [AuthGuard] },
   // {
   //   path: ':challengeName',
   //   loadChildren: () => import('../challenge').then((m) => m.ChallengeModule),
