aba29bf922d42207cbfb1dc0a3b8c9021582aa68
Rongrong Chai <rchai@sagebase.org>
2021-09-15 11:59:55 -0400

    initial design

--- src/app/pages/account/org-account/org-header/org-header.module.ts
+++ src/app/pages/account/org-account/org-header/org-header.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '@shared/material/material.module';
 import { OrgHeaderComponent } from './org-header.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule],
   declarations: [OrgHeaderComponent],
-  exports: [OrgHeaderComponent]
+  exports: [OrgHeaderComponent],
 })
 export class OrgHeaderModule {}
