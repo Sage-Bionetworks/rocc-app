9d72c68d8ab157a276052e10183f70cb7feecd36
Rongrong Chai <rchai@sagebase.org>
2021-09-16 15:49:28 -0400

    improve function to calc progressValue

--- src/app/pages/challenge/challenge-header/challenge-header.component.ts
+++ src/app/pages/challenge/challenge-header/challenge-header.component.ts
@@ -26,26 +26,36 @@ export class ChallengeHeaderComponent implements OnInit {
     this.platform$ = this.challengePlatformService.getChallengePlatform(
       this.challenge?.platformId!
     );
+
+    this.progressValue =
+      this.challenge.status == 'active'
+        ? (this.progressValue =
+            this.challenge.startDate !== undefined &&
+            this.challenge.endDate !== undefined
+              ? this.calcProgress(
+                  new Date().toUTCString(),
+                  this.challenge.startDate!,
+                  this.challenge.endDate!
+                )
+              : 0)
+        : this.challenge.status == 'completed'
+        ? 100
+        : 0;
   }
 
-  getProgress(challenge: Challenge): number {
-    if (challenge.status == 'active') {
-      // if either of start/endDates not provided, still show progress bar, but return zero
-      if (
-        typeof challenge.startDate !== 'undefined' &&
-        typeof challenge.endDate !== 'undefined'
-      ) {
-        const totalTime =
-          +new Date(challenge.endDate!) - +new Date(challenge.startDate!);
-        const runTime = +new Date() - +new Date(challenge.startDate!);
-        this.progressValue = (runTime / totalTime) * 100;
-      }
-    } else {
-      this.progressValue = challenge.status == 'completed' ? 100 : 0;
-    }
-
-    return this.progressValue;
+  calcDays(startDate: string, endDate: string): number {
+    let timeDiff = +new Date(endDate) - +new Date(startDate);
+    return timeDiff / (1000 * 60 * 60 * 24);
   }
+
+  calcProgress(today: string, startDate: string, endDate: string): number {
+    return (
+      (this.calcDays(startDate, today) / this.calcDays(startDate, endDate)) *
+      100
+    );
+  }
+
+  calcRemainDays(today: string, endDate: string): void {}
 
   public toggleSelected() {
     this.isFavorite = !this.isFavorite;
