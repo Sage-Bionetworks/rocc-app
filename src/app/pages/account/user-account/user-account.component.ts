a45ad93bd790e3c79e73a6c9564467ee738a763b
Thomas Schaffter <thomas.schaffter@gmail.com>
2021-09-16 18:01:47 -0700

    List the orgs a user belong to (#235)
    * Use ew method to get orgs of a user
    
    * Retrieve orgs in user-profile component
    
    * List user orgs
    
    * Use routerLink instead of href with mat-nav-list

--- src/app/pages/account/user-account/user-account.component.ts
+++ src/app/pages/account/user-account/user-account.component.ts
@@ -1,19 +1,36 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { UserService, User } from '@sage-bionetworks/rocc-client-angular';
-import { Observable } from 'rxjs';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import {
+  UserService,
+  User,
+  Organization,
+} from '@sage-bionetworks/rocc-client-angular';
+import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'rocc-user-account',
   templateUrl: './user-account.component.html',
   styleUrls: ['./user-account.component.scss'],
 })
-export class UserAccountComponent implements OnInit {
+export class UserAccountComponent implements OnInit, OnDestroy {
   @Input() accountId!: string;
   user$!: Observable<User>;
+  orgs: Organization[] = [];
+  private subscriptions: Subscription[] = [];
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.user$ = this.userService.getUser(this.accountId);
+
+    const orgsSub = this.userService
+      .listUserOrganizations(this.accountId)
+      .pipe(map((page) => page.organizations))
+      .subscribe((orgs) => (this.orgs = orgs));
+    this.subscriptions.push(orgsSub);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 }
