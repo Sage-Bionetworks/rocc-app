4bc5560e269b2bc21af05692db7748516f3ff584
tschaffter <thomas.schaffter@gmail.com>
2021-09-16 21:06:54 -0700

    Improve user profile tabs

--- src/app/pages/account/user-account/user-account.component.ts
+++ src/app/pages/account/user-account/user-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import {
   UserService,
   User,
@@ -6,6 +7,11 @@ import {
 } from '@sage-bionetworks/rocc-client-angular';
 import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
+
+export interface Tab {
+  name: string;
+  visible: boolean;
+}
 
 @Component({
   selector: 'rocc-user-account',
@@ -18,7 +24,38 @@ export class UserAccountComponent implements OnInit, OnDestroy {
   orgs: Organization[] = [];
   private subscriptions: Subscription[] = [];
 
-  constructor(private userService: UserService) {}
+  links = ['overview', 'challenges'];
+
+  // tabs =
+  tabKeys: string[] = [];
+
+  tab$!: Observable<string | null>;
+
+  tabs: { [key: string]: Tab } = {
+    overview: {
+      name: 'Overview',
+      visible: true,
+    },
+    challenges: {
+      name: 'Challenges',
+      visible: true,
+    },
+    starred: {
+      name: 'Starred',
+      visible: false,
+    },
+  };
+
+  activeTab: Tab;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {
+    this.tabKeys = Object.keys(this.tabs);
+    this.activeTab = this.tabs['overview'];
+  }
 
   ngOnInit(): void {
     this.user$ = this.userService.getUser(this.accountId);
@@ -28,9 +65,23 @@ export class UserAccountComponent implements OnInit, OnDestroy {
       .pipe(map((page) => page.organizations))
       .subscribe((orgs) => (this.orgs = orgs));
     this.subscriptions.push(orgsSub);
+
+    const activeTabSub = this.route.queryParamMap
+      .pipe(
+        map((params: ParamMap) => params.get('tab')),
+        map((key) => (key === null ? 'overview' : key))
+      )
+      .subscribe((key) => {
+        if (!this.tabKeys.includes(key)) {
+          this.router.navigate([]);
+        } else {
+          this.activeTab = this.tabs[key];
+        }
+      });
+    this.subscriptions.push(activeTabSub);
   }
 
   ngOnDestroy(): void {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 }
---
+++ src/app/pages/account/user-account/user-profile-starred/user-profile-starred.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rocc-user-profile-starred',
+  templateUrl: './user-profile-starred.component.html',
+  styleUrls: ['./user-profile-starred.component.scss']
+})
+export class UserProfileStarredComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
