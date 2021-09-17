7a941f390c7efa13a8416efb9f4e97e6c4c572fb
Rongrong Chai <73901500+rrchai@users.noreply.github.com>
2021-09-17 17:48:28 -0400

    Update organization page header (#228)
    * initial design
    
    * remove last update
    
    * make mock up data to show org logo
    
    * change font size
    
    * update layout
    
    * update-org-header
    
    * update image location
    
    * fix api query of  number of people
    
    * update api query on number of challenges
    
    * add short description and update layout
    
    * add tooltip on link
    
    * display web url and email
    
    * add org data/logo
    
    * update images
    
    * fix org data
    
    * update layout
    
    * change branch back to main on json
    
    * correct sage's image name in organizations.json
    
    * use totalResults to get number of challenges
    
    Co-authored-by: Thomas Schaffter <thomas.schaffter@gmail.com>
    
    Co-authored-by: Thomas Schaffter <thomas.schaffter@gmail.com>

--- src/app/pages/account/org-account/org-header/org-header.component.ts
+++ src/app/pages/account/org-account/org-header/org-header.component.ts
@@ -1,13 +1,53 @@
-import { Component, Input } from '@angular/core';
-import { Organization } from '@sage-bionetworks/rocc-client-angular';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  ChallengeService,
+  OrgMembershipService,
+  Organization,
+} from '@sage-bionetworks/rocc-client-angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { map as _map, uniqBy as _uniqBy } from 'lodash-es';
 
 @Component({
   selector: 'rocc-org-header',
   templateUrl: './org-header.component.html',
   styleUrls: ['./org-header.component.scss'],
 })
-export class OrgHeaderComponent {
+export class OrgHeaderComponent implements OnInit {
   @Input() org!: Organization;
+  numChallenges$!: Observable<number>;
+  numPeople$!: Observable<number>;
 
-  constructor() {}
+  // mock data
+  // TODO: replace mock data with 'org' when needed properties can be retrieve
+  // tmpOrg = {
+  //   id: '613931c6bef3ffc6e5091e4b',
+  //   email: 'contact@example.org',
+  //   login: 'sage-bionetworks',
+  //   name: 'Sage Bionetworks',
+  //   avatarUrl: 'assets/img/sage-bionetworks.png',
+  //   websiteUrl: 'https://sagebionetworks.org/',
+  //   description: 'This challenge is awesome for short description',
+  // };
+
+  constructor(
+    private challengeService: ChallengeService,
+    private orgMembershipService: OrgMembershipService
+  ) {}
+
+  ngOnInit(): void {
+    this.numChallenges$ = this.challengeService
+      .listAccountChallenges(this.org.login, 50, 0)
+      .pipe(map((page) => page.totalResults));
+
+    this.numPeople$ = this.orgMembershipService
+      .listOrgMemberships(50, 0, this.org.id, undefined)
+      .pipe(
+        map((page) => page.orgMemberships),
+        map((orgMemberships) =>
+          _map(_uniqBy(orgMemberships, 'userId'), 'userId')
+        ),
+        map((userIds) => (userIds === undefined ? 0 : userIds.length))
+      );
+  }
 }
