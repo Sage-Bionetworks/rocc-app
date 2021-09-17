7b144ada938b8e0c64118cdabc629886f3e1a22b
Thomas Schaffter <thomas.schaffter@gmail.com>
2021-09-16 21:56:47 -0700

    Show the challenges starred by a user (#238)
    * Improve user profile tabs
    
    * First version of list of starred challenges of a user
    
    * Fix / issue with routerLink
    
    * Replace ngIf by ngSwitch

--- src/app/pages/account/user-account/user-account.component.ts
+++ src/app/pages/account/user-account/user-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import {
   UserService,
   User,
@@ -6,6 +7,8 @@ import {
 } from '@sage-bionetworks/rocc-client-angular';
 import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Tab } from './tab.model';
+import { TABS } from './user-account-tabs';
 
 @Component({
   selector: 'rocc-user-account',
@@ -16,9 +19,16 @@ export class UserAccountComponent implements OnInit, OnDestroy {
   @Input() accountId!: string;
   user$!: Observable<User>;
   orgs: Organization[] = [];
+  tabs = TABS;
+  tabKeys: string[] = Object.keys(this.tabs);
+  activeTab: Tab = this.tabs['overview'];
   private subscriptions: Subscription[] = [];
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.user$ = this.userService.getUser(this.accountId);
@@ -28,9 +38,23 @@ export class UserAccountComponent implements OnInit, OnDestroy {
       .pipe(map((page) => page.organizations))
       .subscribe((orgs) => (this.orgs = orgs));
     this.subscriptions.push(orgsSub);
+
+    const activeTabSub = this.route.queryParamMap
+      .pipe(
+        map((params: ParamMap) => params.get('tab')),
+        map((key) => (key === null ? 'overview' : key))
+      )
+      .subscribe((key) => {
+        if (!this.tabKeys.includes(key)) {
+          this.router.navigate([]);
+        } else {
+          this.activeTab = this.tabs[key];
+        }
+      });
+    this.subscriptions.push(activeTabSub);
   }
 
   ngOnDestroy(): void {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 }
