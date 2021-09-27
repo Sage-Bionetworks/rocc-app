0b6d7e813cf68e4279285ddf5531ae910bb38c6e
Rongrong Chai <rchai@sagebase.org>
2021-09-26 19:32:32 -0500

    make simple org card

--- src/app/shared/cards/org-card/org-card.component.ts
+++ src/app/shared/cards/org-card/org-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Organization } from '@sage-bionetworks/rocc-client-angular';
 
 @Component({
   selector: 'rocc-org-card',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./org-card.component.scss'],
 })
 export class OrgCardComponent implements OnInit {
+  @Input() org!: Organization;
   constructor() {}
 
   ngOnInit(): void {}
