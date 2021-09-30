b019e8b008de48e98331dc96f9ebb174e0e4dfac
Rongrong Chai <73901500+rrchai@users.noreply.github.com>
2021-09-29 21:27:09 -0400

    Structure user overview (#300)
    * add org-card component
    
    * make simple org card
    
    * add layout on overview
    
    * use avatar if not img in db
    
    * add link to title only
    
    * adjust overview title size

---
+++ src/app/shared/cards/org-card/org-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Organization } from '@sage-bionetworks/rocc-client-angular';
+import { Avatar } from '@sage-bionetworks/sage-angular';
+
+@Component({
+  selector: 'rocc-org-card',
+  templateUrl: './org-card.component.html',
+  styleUrls: ['./org-card.component.scss'],
+})
+export class OrgCardComponent implements OnInit {
+  @Input() org!: Organization;
+  @Input() orgAvatar!: Avatar;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.orgAvatar = {
+      name: this.org.name
+        ? (this.org.name as string)
+        : this.org.login.replace(/-/g, ' '),
+      src: this.org.avatarUrl!,
+      size: 100,
+    };
+  }
+}
