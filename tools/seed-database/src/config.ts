ddea9fd39607ae653eb11dfc3bc9de0bdd304e1d
tschaffter <thomas.schaffter@gmail.com>
2021-10-07 11:17:26 -0700

    Add dropCollections function

--- tools/seed-database/src/config.ts
+++ tools/seed-database/src/config.ts
@@ -5,6 +5,7 @@ export const config = {
     options: {
       user: process.env.DB_USERNAME || 'roccmongo',
       pass: process.env.DB_PASSWORD || 'roccmongo'
-    }
+    },
+    uri: process.env.DB_URI || 'mongodb://localhost:27017/rocc'
   },
 };
--- tools/seed-database/src/index.ts
+++ tools/seed-database/src/index.ts
@@ -5,47 +5,60 @@ import { AccountModel } from './models';
 import userList from './seeds/development/users.json';
 
 
-let message: string = 'Hello World';
-console.log(message);
+const mongooseConnection = mongoose.connect(config.mongo.uri, config.mongo.options);
+mongoose.connection.on('error', err => {
+    console.error(`MongoDB connection error: ${err}`);
+    process.exit(-1);
+});
 
-console.log(config);
-
-
-console.log(userList.users);
-
-// let user: User = {
-//   id: '615e5b572797c126680142e5',
-//   login: 'tschaffter',
-//   email: '',
-//   createdAt: '',
-//   updatedAt: '',
-//   type: 'User'
-// };
-// console.log(user);
-
-async function run(): Promise<void> {
-
-  const dbUri = 'mongodb://localhost:27017/rocc';
-  await mongoose.connect(dbUri, config.mongo.options);
-
-  // await StarredChallenge.find({})
-  //   .deleteMany()
-  //   .catch(err => console.log('Error populating starred challenges', err));
-
-  // const doc = new UserModel({
-  //   name: 'Bill',
-  //   email: 'bill@initech.com',
-  //   avatar: 'https://i.imgur.com/dM7Thhn.png'
-  // });
-  // await doc.save();
-
-  // console.log(doc.email); // 'bill@initech.com'
+function dropCollections() {
+  const db: any = mongoose.connection.db;
+  return db.listCollections().toArray()
+    .then((collections: any[]) => collections.map((collection) => db.dropCollection(collection.name)))
+    .then((promises: Promise<boolean>[]) => Promise.all(promises))
 }
 
-run()
-  .then(plop => console.log('Connected to DB', plop))
-  .then(() => {
-    return AccountModel.find({})
-      .deleteMany();
-  })
+mongooseConnection
+  .then(() => console.log('Connected to DB'))
+  .then(dropCollections)
   .catch(err => console.log(err));
+
+
+
+
+// async function run(): Promise<void> {
+
+//   const dbUri = 'mongodb://localhost:27017/rocc';
+//   await mongoose.connect(dbUri, config.mongo.options);
+
+//   const db: any = mongoose.connection.db;
+//   const collections: any[] = await db.listCollections().toArray();
+
+//   console.log('collection', collections);
+//   let promises: Promise<boolean>[] = collections
+//     .map((collection) => db.dropCollection(collection.name));
+
+//   await Promise.all(promises);
+
+
+//   // await StarredChallenge.find({})
+//   //   .deleteMany()
+//   //   .catch(err => console.log('Error populating starred challenges', err));
+
+//   // const doc = new UserModel({
+//   //   name: 'Bill',
+//   //   email: 'bill@initech.com',
+//   //   avatar: 'https://i.imgur.com/dM7Thhn.png'
+//   // });
+//   // await doc.save();
+
+//   // console.log(doc.email); // 'bill@initech.com'
+// }
+
+// run()
+//   .then(() => console.log('Connected to DB'))
+//   // .then(() => {
+//   //   return AccountModel.find({})
+//   //     .deleteMany();
+//   // })
+//   .catch(err => console.log(err));
