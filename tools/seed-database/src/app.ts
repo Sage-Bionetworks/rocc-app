c7afa2cde843d7587c40abb8e521ac2244d024aa
tschaffter <thomas.schaffter@gmail.com>
2021-10-07 14:53:36 -0700

    Add options to CLI

--- tools/seed-database/src/app.ts
+++ tools/seed-database/src/app.ts
@@ -4,20 +4,25 @@ import { dropCollections } from './database';
 import { config } from './config';
 
 export class App {
-  private program;
+  private program: any;
 
   constructor() {
     this.program = new Command();
 
     this.program
-      .option('-d, --debug', 'output extra debugging')
-      .option('-s, --small', 'small pizza size')
-      .option('-p, --pizza-type <type>', 'flavour of pizza');
+      .option('--uri <uri>', 'MongoDB uri', 'mongodb://localhost:27017/rocc')
+      .option('--username <username>', 'MongoDB username', 'roccmongo')
+      .option('--password <password>', 'MongoDB password', 'roccmongo')
+      .option('--input <path>', 'directory that contains the JSON files');
   }
 
   public run(): void {
     this.program.parse(process.argv);
 
+    const options = this.program.opts();
+    console.log('options:', options);
+
+    // config.mongo.uri =
 
     // const mongooseConnection = connect(config.mongo.uri, config.mongo.options);
     // connection.on('error', (err: any) => {
