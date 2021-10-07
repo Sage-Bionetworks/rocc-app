b17a492103759f6b1e9e46a8d304e25b58116be3
tschaffter <thomas.schaffter@gmail.com>
2021-10-06 19:30:20 -0700

    Add local @sage-bionetworks/rocc-client-angular

--- tools/seed-database/src/index.ts
+++ tools/seed-database/src/index.ts
@@ -1,4 +1,15 @@
 import { Schema, model, connect } from 'mongoose';
+import { User } from "@sage-bionetworks/rocc-client-angular"
 
 let message: string = 'Hello World';
 console.log(message);
+
+let user: User = {
+  id: '615e5b572797c126680142e5',
+  login: 'tschaffter',
+  email: '',
+  createdAt: '',
+  updatedAt: '',
+  type: 'User'
+};
+console.log(user);
