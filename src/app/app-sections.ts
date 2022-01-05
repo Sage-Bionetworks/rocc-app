ffd7e9920c94505f198580d8f02bacad6fc52098
Verena Chung <9377970+vpchung@users.noreply.github.com>
2022-01-05 10:27:16 -0800

    Add 'About' section to the app (#358)
    * add intial About component, module, and routing
    
    * add text
    
    * add initial styling; FIXME later
    
    * remove spaces
    
    * remove more spaces

--- src/app/app-sections.ts
+++ src/app/app-sections.ts
@@ -2,6 +2,10 @@
 import { Section } from '@sage-bionetworks/sage-angular/src/lib/navbar/section';
 
 export const SECTIONS: { [key: string]: Section } = {
+  about: {
+    name: 'About',
+    summary: 'About ROCC'
+  },
   search: {
     name: 'Search',
     summary: 'Search challenges'
---
+++ src/app/pages/about/about.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AvatarModule, FooterModule } from '@sage-bionetworks/sage-angular';
+import { AboutComponent } from './about.component';
+import { MaterialModule } from '@shared/material/material.module';
+
+
+
+const routes: Routes = [
+  { path: '', component: AboutComponent }
+];
+
+@NgModule({
+  declarations: [AboutComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FooterModule,
+    MaterialModule,
+    AvatarModule,
+  ],
+  exports: [AboutComponent]
+})
+export class AboutModule { }
