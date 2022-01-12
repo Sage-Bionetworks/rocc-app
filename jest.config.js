ac25dcbe1c5d18e8e2bead66ea0f4228577cce40
Rongrong Chai <rchai@sagebase.org>
2022-01-12 10:17:25 -0500

    fix app...spec.ts and update jest.config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@ module.exports = {
   preset: 'jest-preset-angular',
   roots: ['<rootDir>/src/'],
   setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
+  },
+  transformIgnorePatterns: ['node_modules/(?!lodash-es/.*|.*\\.mjs)'],
 };
