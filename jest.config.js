99f8eec324c14b88470465d2fa43dd4c6fddf92f
Rongrong Chai <rchai@sagebase.org>
2022-02-09 11:53:42 -0500

    install and set up jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  roots: ['<rootDir>/src/'],
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
+    '^@angular/(.*)$': '<rootDir>/node_modules/@angular/$1',
+  },
+  transformIgnorePatterns: ['node_modules/(?!lodash-es/.*|.*\\.mjs)'],
+};
