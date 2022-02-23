898125eeabe34b4183894bef9d78b3e8ae9343a1
Rongrong Chai <rchai@sagebase.org>
2022-02-23 09:38:50 -0500

    setup

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,11 @@ module.exports = {
     '^@app/(.*)$': '<rootDir>/src/app/$1',
     '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
     '^@angular/(.*)$': '<rootDir>/node_modules/@angular/$1',
+    '^@sage-bionetworks/(.*)$': '<rootDir>/node_modules/@sage-bionetworks/$1',
   },
   transformIgnorePatterns: ['node_modules/(?!lodash-es/.*|.*\\.mjs)'],
+  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
 };
+// moduleNameMapper: {
+//   '^lodash-es$': 'lodash'
+// },
--- src/app/pages/search/challenge-search/challenge-search.component.spec.ts
+++ src/app/pages/search/challenge-search/challenge-search.component.spec.ts
@@ -1,25 +1,48 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { ChallengeSearchComponent } from './challenge-search.component';
+import { ChallengeSearchComponent } from './challenge-search.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { AvatarModule } from '@sage-bionetworks/sage-angular/src/lib/avatar';
+import { FooterModule } from '@sage-bionetworks/sage-angular';
+import { MaterialModule } from '@shared/material/material.module';
+import { FiltersModule } from '@shared/filters/filters.module';
+import { SearchRoutingModule } from '../search-routing.module';
+import { SearchComponent } from '../search.component';
+import { ChallengeCardModule } from '@app/shared/cards/challenge-card/challenge-card.module';
+import { PrimengModule } from '@shared/primeng/primeng.module';
 
-// describe('ChallengeSearchComponent', () => {
-//   let component: ChallengeSearchComponent;
-//   let fixture: ComponentFixture<ChallengeSearchComponent>;
+describe('ChallengeSearchComponent', () => {
+  let component: ChallengeSearchComponent;
+  let fixture: ComponentFixture<ChallengeSearchComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ChallengeSearchComponent]
-//     })
-//       .compileComponents();
-//   });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ChallengeSearchComponent, SearchComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        CommonModule,
+        FooterModule,
+        MaterialModule,
+        FiltersModule,
+        ChallengeCardModule,
+        SearchRoutingModule,
+        PrimengModule,
+        AvatarModule,
+      ],
+      providers: [HttpClient],
+    }).compileComponents();
+  });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ChallengeSearchComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChallengeSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
