e6ae2b79b9836f220f7be7d148d0cc633759d97e
Rongrong Chai <rchai@sagebase.org>
2022-02-28 09:43:46 -0500

    test on challenge-card

--- jest.config.js
+++ jest.config.js
@@ -7,11 +7,18 @@ module.exports = {
     '^@app/(.*)$': '<rootDir>/src/app/$1',
     '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
     '^@angular/(.*)$': '<rootDir>/node_modules/@angular/$1',
-    '^@sage-bionetworks/(.*)$': '<rootDir>/node_modules/@sage-bionetworks/$1',
+    '^@sage-bionetworks/sage-angular/(.*)$':
+      '<rootDir>/sage-angular/dist/sage-angular/$1',
+    '^@sage-bionetworks/rocc-client-angular/(.*)$':
+      '<rootDir>/sage-angular/dist/sage-angular/$1',
   },
   transformIgnorePatterns: ['node_modules/(?!lodash-es/.*|.*\\.mjs)'],
   moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+  moduleDirectories: ['node_modules', 'src'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['src/app/**/*.spec.ts'],
+  coverageReporters: ['text'],
 };
-// moduleNameMapper: {
-//   '^lodash-es$': 'lodash'
-// },
--- src/app/shared/cards/challenge-card/challenge-card.component.spec.ts
+++ src/app/shared/cards/challenge-card/challenge-card.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ChallengeCardComponent } from './challenge-card.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterModule } from '@sage-bionetworks/sage-angular';
 
 describe('CardPreviewComponent', () => {
   let component: ChallengeCardComponent;
@@ -11,7 +12,7 @@ describe('CardPreviewComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ChallengeCardComponent],
-      imports: [RouterTestingModule, HttpClientModule],
+      imports: [RouterTestingModule, HttpClientModule, FooterModule],
     }).compileComponents();
   });
 
